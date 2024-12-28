0591f92f3b89779e93ffe65fbc94c31c428d5f46
remote007 <pmnrai2020@gmail.com>
2024-12-28 14:23:34 +0530

    navbar

---
+++ scripts/baseURL.js
@@ -0,0 +1,1 @@
+export const baseURL = "https://indecisive-coherent-scar.glitch.me"
---
+++ scripts/navbar.js
@@ -0,0 +1,6 @@
+
+const navbar = `<a href = "index.html">HOME</a>
+<a href = "books.html">BOOKS</a>
+<a href = "admin.html">ADMIN</a>`
+;
+document.getElementById("nav").innerHTML= navbar
